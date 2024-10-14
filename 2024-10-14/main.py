ff99858d93b6cbef75bf5795403d15393ff4d265
vilius.ramulionis <vilius.ramulionis@gmail.com>
2024-10-14 21:59:59 +0300

    2024-10-14

--- 2024-10-14/main.py
+++ 2024-10-14/main.py
@@ -33,3 +33,24 @@ pavadinimas = 'UAB "Gera įmonė"'
 
 print(pavadinimas)
 
+# SPRENDIMAI:
+
+print(8 * 4 + 2)
+print(8 * (4 + 2))
+print(48 / 4)
+print(3 + 6 * 2)
+
+pirmasSkaicius = 4
+antrasSkaicius = 15
+rezultatas = pirmasSkaicius + antrasSkaicius
+
+# norint sujungti du stringus į vieną yra naudojamas + operatorius
+# str() yra funkcija kuri konvertuoja duomenį į str duomens tipą
+
+print("Rezultatas: " + str(rezultatas))
+
+# Kėlimas laipsniu:
+
+rezultatas = 5 ** 2
+
+print(rezultatas)
