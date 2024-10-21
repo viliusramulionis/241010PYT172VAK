64d74aafd8bc95fafdd8ab71172c0ced02835138
vilius.ramulionis <vilius.ramulionis@gmail.com>
2024-10-21 20:12:22 +0300

    if else

---
+++ 2224-10-21/main.py
@@ -0,0 +1,117 @@
+# Taip / Ne - Boolean
+
+# teigiamas = True
+# neigiamas = False
+
+tikrinimas = True
+
+print(type(tikrinimas))
+
+if tikrinimas: 
+    print("Kintamasis yra teigiamas")
+else:
+    print("Kintamasis yra neigiamas")
+
+print("Patikrinimas baigėsi")
+
+
+# Sukurkite du kintamuosiuos, priskirkite jiems 
+# norimas reikšmes ir didesnę reikšmę padalinkite iš mažesnės
+
+pirmas = 8
+antras = 12
+
+if pirmas > antras : 
+    # False
+    print(pirmas / antras)
+elif pirmas == antras :
+    # True 
+    print("Skaičiai yra lygūs")
+else :
+    print(antras / pirmas)
+
+
+# Elif alternatyva:
+if pirmas > antras :
+    print(pirmas / antras)
+else :
+    if pirmas == antras:
+         print("Skaičiai yra lygūs")
+    else:
+        print(antras / pirmas)
+
+
+skaicius = 15
+
+if skaicius == 15 :
+    print("Skaičius yra lygus penkiolikai")
+
+
+procentas = 79
+
+# Kai naudojamas and operatorius visos sąlygos privalo būti teigiamos
+if procentas >= 80 and procentas <= 90 :
+    print("Procentas papuola į A kategoriją")
+
+
+skaicius = 7
+
+# Naudojant or operatorių, tik viena iš išvardintų sąlygų turi būti teigiama
+if skaicius == 1 or skaicius == 4 or skaicius == 7 or skaicius == 10 :
+    print("Jūs laimėjote")
+
+
+skaicius = 10
+
+if skaicius > 5 and skaicius > 8:
+    print("Salyga teigiama")
+
+
+stringas = "Labadienavarototojauusername511515"
+
+if stringas.isalnum() :
+    print("Stringas neturi specialiųjų simbolių")
+
+stringas = "labas"
+
+if stringas.isalpha() :
+    print("Stringas yra tik iš raidžių")
+
+skaicius = input("Įveskite skaičių:")
+
+# TERNARY OPERATORIAUS PANAUDOJIMAS
+
+# Sąlygos aprašymas nenaudojant ternary operatoriaus
+# if skaicius.isnumeric() :
+#     skaicius = int(skaicius)
+# else :
+#     skaicius = 0
+
+# print(skaicius)
+
+# Su ternary operatoriumi
+
+skaicius = int(skaicius) if skaicius.isnumeric() else 0
+
+print(skaicius)
+
+# Tikriname stringus:
+
+tekstas = ''
+
+if tekstas :
+    # False dėl to nieko nevyksta
+    print("Stringas nėra tuščias")
+
+skaicius = 0
+
+if skaicius :
+    # False dėl to nieko nevyksta
+    print("Skaičius yra teigiamas")
+
+vardas = input("Įveskite vardą: ")
+
+if vardas :
+    print("Įvestas vardas: ", vardas)
+else :
+    vardas = input("Įveskite vardą: ")
---
+++ 2224-10-21/sprendimai.py
@@ -0,0 +1,12 @@
+sarasas = ["Vilnius", 1323, ["Nupirkti maisto", "PABėgioti"], 8, "Rytas"]
+
+# • Išveskite Vilniaus paminėjimo metus, žodį ”Vilnius” didžiosiomis raidėmis
+
+print(sarasas[1])
+print(sarasas[0].upper())
+
+# Išveskite žodžio ”maisto” paskutines tris raides didžiosiomis.
+zodis = sarasas[2][0]
+print(zodis[-3:].upper())
+
+# print(sarasas[2][0])
