4a3f66e60198af5f0d47a50388d742d86a588453
vilius.ramulionis <vilius.ramulionis@gmail.com>
2024-11-14 22:06:32 +0200

    televizoriaus klase

---
+++ 2024-11-14/televizorius.py
@@ -0,0 +1,36 @@
+class Televizorius() :
+    kanalas = 1
+    garsas = 50
+
+    def __init__(self, gamintojas) :
+        self.gamintojas = gamintojas
+
+    def reiksmes(self) :
+        return f"Televizorius ‘{self.gamintojas}’ šiuo metu rodo {self.kanalas} kanalą. Garso lygis {self.garsas}."
+    
+    def set_garsas(self, garsas) :
+        if garsas <= 100 and garsas >= 0 :
+            self.garsas = garsas
+
+    def set_kanalas(self, kanalas) :
+        if kanalas <= 50 and kanalas > 0 :
+            self.kanalas = kanalas
+        else :
+            self.kanalas = 1
+
+    
+sony = Televizorius("Sony")
+
+sony.set_garsas(85)
+sony.set_garsas(-5)
+sony.set_kanalas(24)
+
+print(sony.reiksmes())
+
+lg = Televizorius("LG")
+
+lg.set_garsas(12)
+lg.set_kanalas(49)
+lg.set_kanalas(0)
+
+print(lg.reiksmes())
